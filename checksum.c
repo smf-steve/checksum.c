795b70dde07b2f5dac5048aab51f8b8879c36bd7
Steven Fitzgerald <steve@csun.edu>
2021-02-16 10:36:55 -0800

    starting point

--- checksum.c
+++ checksum.c
@@ -1,15 +1,32 @@
+/********************************/
+/* Program Name: asdl;kfj         */
+/* Author:       Steven               */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
 
 #include "stdio.h"
 #include "stdlib.h"
 
 #define max_int (255)
-#define byte (char)
+#define byte unsigned char
 
 int main (int argc, char * argv[], char ** envp) {
 
-   printf("Hello World!\n);
+  int count = 10;
+  int sum = 0;   
+  byte checksum; 
+  byte complement;
 
+  /* the following is the prototype for the read system call */
+  /* int read(int fildes, void *buf, size_t nbyte);  */
+  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  if (checksum != complement ) {
+    fprintf(stderr, "Error Detected!\n"); 
+    return 1;
+  }
+  return 0;
 }
-
-
-
